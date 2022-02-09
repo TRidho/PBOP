73990b0fe4622268cb86119bbba3c8aba0e7df81
TRidho <99310493+TRidho@users.noreply.github.com>
2022-02-09 18:57:13 +0700

    Add files via upload

---
+++ w1_pbop.py
@@ -0,0 +1,9 @@
+#5210411208_MUHAMMAD TAUFIK RIDHO
+#Menentukan Luas Segitiga
+
+alas=int(input("Alas : "))
+tinggi =int(input("Tinggi : "))
+
+luas = 1/2 * (alas * tinggi)
+
+print("Luas Segitiga =", luas)
