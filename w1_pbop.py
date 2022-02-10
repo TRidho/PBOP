97d027a7d16ecf70b94af7787c36a1af96833747
TRidho <99310493+TRidho@users.noreply.github.com>
2022-02-10 09:07:06 +0700

    Update w1_pbop.py

--- w1_pbop.py
+++ w1_pbop.py
@@ -1,8 +1,26 @@
 #5210411208_MUHAMMAD TAUFIK RIDHO
 #Menghitung Luas Segitiga
-print("Menghitung Luas Segitiga")
-alas=float(input("Alas : "))
-tinggi =float(input("Tinggi : "))
+while True :
+    print("---Menu---")
+    print("1.Menghitung Luas Segitiga.")
+    print("2.Menghitung Keliling Segitiga.")
+    print("0.Selesai.")
+    pilih = input("Masukkan Pilihan : ")
 
-luas = 1/2*alas*tinggi
-print("Luas Segitiga =", luas)
+    if pilih == '1':
+        alas=float(input("Alas : "))
+        tinggi=float(input("Tinggi : "))
+
+        luas = 1/2*alas*tinggi
+        print("Luas Segitiga =", luas)
+    
+    elif pilih == '2':
+        s1=float(input("Sisi Pertama : "))
+        s2=float(input("Sisi Kedua : "))
+        s3=float(input("Sisi Ketiga : "))
+
+        keliling = s1+s2+s3
+        print("Luas Segitiga =", keliling)
+    else:
+        print('Terimakasih')
+        break
